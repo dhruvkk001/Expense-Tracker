0f24510b12cb112e0df3571d000bf9430177e463
Dhruv Kumar <dhruvkk001@gmail.com>
2024-01-14 21:09:59 +0530

    worked on frontend

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+        <h6 className='text-center'> All Rights Reserved &copy; Dhruv Kumar</h6>
+        
+    </div>
+  )
+}
+
+export default Footer
